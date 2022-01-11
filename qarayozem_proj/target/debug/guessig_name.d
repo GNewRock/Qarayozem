c670b07c1fc55659c9b05929212316b094464925
Ildar Safin(Safa) <89279583+GNewRock@users.noreply.github.com>
2022-01-11 10:18:19 +0400

    delite

--- qarayozem_proj/target/debug/guessig_name.d
+++
@@ -1,1 +0,0 @@
-/home/hacker/Projects/Qarayozem/guessig_name/target/debug/guessig_name: /home/hacker/Projects/Qarayozem/guessig_name/src/main.rs
--- qarayozem_proj/target/debug/qarayozem_proj.d
+++
@@ -1,1 +0,0 @@
-/home/hacker/Projects/Qarayozem/qarayozem_proj/target/debug/qarayozem_proj: /home/hacker/Projects/Qarayozem/qarayozem_proj/src/main.rs
